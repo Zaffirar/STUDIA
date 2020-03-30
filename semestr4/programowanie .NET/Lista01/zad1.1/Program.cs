058d4b9e7a0d3f04af1efa6c6dbb90b108e0ec8f
Wiktor Hamberger <45981365+Zaffirar@users.noreply.github.com>
2020-03-30 17:05:52 +0200

    .Net lista01

--- semestr4/programowanie .NET/Lista01/zad1.1/Program.cs
+++ semestr4/programowanie .NET/Lista01/zad1.1/Program.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 
 namespace zad1._1
 {
-
+    /// <summary>
+    /// Główna klasa programu
+    /// </summary>
     public class CMain
     {
+        /// <summary>
+        /// Metoda przyjmuje liczbę i zwraca listę jej cyfr i na koniec listy dodaje sumę cyfr tej liczby 
+        /// </summary>
+        /// <param name="a">liczba do rozłożenia</param>
+        /// <returns>Lista cyfr i sumy cyfr liczby a</returns>
         public static List<int> ListaISumaCyfr(int a)
         {
             List<int> wynik = new List<int>();
@@ -13,16 +20,16 @@ namespace zad1._1
             while (a > 0)
             {
                 int modu = a % 10;
-                if (modu != 0)
-                {
-                    wynik.Add(modu);
-                    suma += modu;
-                }
+                wynik.Add(modu);
+                suma += modu;
                 a /= 10;
             }
             wynik.Add(suma);
             return wynik;
         }
+        /// <summary>
+        /// Główna metoda programu
+        /// </summary>
         public static void Main()
         {
             List<int> wynik = new List<int>();
@@ -32,7 +39,7 @@ namespace zad1._1
                 dobrze = true;
                 foreach (int x in ListaISumaCyfr(i))
                 {
-                    if (i % x != 0)
+                    if (x == 0 || i % x != 0)
                     {
                         dobrze = false;
                         break;
