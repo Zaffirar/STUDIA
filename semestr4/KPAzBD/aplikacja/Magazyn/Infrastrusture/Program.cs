8259b5398eb27adcc306e8da23b81068eab827bf
Zaffirar <wiktor1999@vp.pl>
2020-05-14 22:02:45 +0200

    rpis ex10set9

--- semestr4/KPAzBD/aplikacja/Magazyn/Infrastrusture/Program.cs
+++ semestr4/KPAzBD/aplikacja/Magazyn/Infrastrusture/Program.cs
@@ -4,7 +4,7 @@ namespace Infrastrusture
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
         }
